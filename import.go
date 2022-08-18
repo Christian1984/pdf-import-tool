d0aa7e39cf862d4ef0967a3d2dfd08f16f4ec347
Christian Hoffmann <chris.hoffmann84@gmail.com>
2022-08-18 11:19:21 +0200

    various fixes

--- import.go
+++ import.go
@@ -3,28 +3,15 @@ package main
 import (
 	"errors"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
 )
 
-var importerBasePath, _ = filepath.Abs("pdf-importer")
-var importerCallerPath, _ = filepath.Abs(importerBasePath + "\\" + "pdf-import-runner.exe")
+var importerBasePath = AbsLibPath
 var importerExePath, _ = filepath.Abs(importerBasePath + "\\" + "gswin64c.exe")
 var importerDllPath, _ = filepath.Abs(importerBasePath + "\\" + "gsdll64.dll")
-var importerLicensePath, _ = filepath.Abs(importerBasePath + "\\" + "THIRD-PARTY-LICENSE.md")
-
-const importerBaseUrl = "https://github.com/Christian1984/pdf-importer/releases/download/v1.1.1/"
-const importerCallerUrl = importerBaseUrl + "pdf-import-runner.exe"
-const importerExeUrl = importerBaseUrl + "gswin64c.exe"
-const importerDllUrl = importerBaseUrl + "gsdll64.dll"
-const importerLicenseUrl = importerBaseUrl + "THIRD-PARTY-LICENSE.md"
-
-const sourceFolder = "charts\\!import"
-const outFolder = "charts\\imported"
 
 type PdfFileInfo struct {
 	SourcePath string
@@ -37,10 +24,10 @@ func importPdfChart(sourcePath string, targetBasePath string, filename string) e
 
 	documentTargetPath, _ := filepath.Abs(targetBasePath + "\\" + strings.TrimSuffix(filename, ".pdf"))
 
-	in, _ := filepath.Abs(sourcePath + "\\" + filename)
-	out, _ := filepath.Abs(documentTargetPath + "\\" + strings.TrimSuffix(filename, ".pdf") + "-%03d.png")
+	absIn, _ := filepath.Abs(sourcePath + "\\" + filename)
+	absOut, _ := filepath.Abs(documentTargetPath + "\\" + strings.TrimSuffix(filename, ".pdf") + "-%03d.png")
 
-	fmt.Println("Starting PDF Import of " + in)
+	fmt.Println("Starting PDF Import of " + absIn)
 	fmt.Println("Creating out path at " + documentTargetPath)
 	mkDirErr := os.MkdirAll(documentTargetPath, os.ModePerm)
 
@@ -50,16 +37,26 @@ func importPdfChart(sourcePath string, targetBasePath string, filename string) e
 	}
 
 	cmdParams := []string{
-		"--out",
-		out,
-		"--in",
-		in,
-		"--gspath",
-		".\\pdf-importer",
-		"--verbose",
+		//"-q",
+		//"-dQUIET",
+		"-dSAFER",
+		"-dBATCH",
+		"-dNOPAUSE",
+		"-dNOPROMPT",
+		"-dMaxBitmap=500000000",
+		"-dAlignToPixels=0",
+		"-dGridFitTT=2",
+		"-sDEVICE=png16m",
+		"-dTextAlphaBits=4",
+		"-dGraphicsAlphaBits=4",
+		"-r600",
+
+		"-o",
+		absOut,
+		absIn,
 	}
 
-	cmd := exec.Command(importerCallerPath, cmdParams...)
+	cmd := exec.Command(importerExePath, cmdParams...)
 	fmt.Println("Import command is: " + cmd.String())
 
 	s, importErr := cmd.Output()
@@ -91,45 +88,24 @@ func fileExists(filePath string) bool {
 	return true
 }
 
-func downloadFile(filepath string, url string) error {
-	fmt.Println("Downloading from " + url + " to " + filepath)
-
-	// Get the data
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	// Create the file
-	out, err := os.Create(filepath)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	// Write the body to file
-	_, err = io.Copy(out, resp.Body)
-	return err
-}
-
 func createAndOpenFolder(path string) {
 	absPath, _ := filepath.Abs(path)
 	os.MkdirAll(absPath, os.ModePerm)
 
-	fmt.Println("Trying to open folder [" + path + "]...")
-	err := OpenExplorer(path)
+	fmt.Println("Trying to open folder [" + absPath + "]...")
+	cmd := exec.Command("explorer", absPath)
+	cmd.Run()
 
-	if err != nil {
-		fmt.Println("Could not open folder [" + path + "]")
-		ShowError("Folder could not be opened! Reason: " + err.Error())
-	}
+	// if err != nil {
+	// fmt.Println("Could not open folder [" + path + "]")
+	// ShowError("Folder could not be opened! Reason: " + err.Error())
+	// }
 }
 
 func CreatePdfFileList() ([]PdfFileInfo, error) {
 	list := []PdfFileInfo{}
 
-	absPath, _ := filepath.Abs(sourceFolder)
+	absPath := AbsInputRoot
 	sourceFolderErr := os.MkdirAll(absPath, os.ModePerm)
 
 	if sourceFolderErr != nil {
@@ -137,7 +113,7 @@ func CreatePdfFileList() ([]PdfFileInfo, error) {
 		return list, sourceFolderErr
 	}
 
-	walkErr := filepath.Walk(sourceFolder, func(filePath string, info os.FileInfo, err error) error {
+	walkErr := filepath.Walk(AbsInputRoot, func(filePath string, info os.FileInfo, err error) error {
 		if info.IsDir() {
 			fmt.Println("%v", info)
 		} else {
@@ -149,13 +125,13 @@ func CreatePdfFileList() ([]PdfFileInfo, error) {
 
 			fmt.Println("%v", info)
 			sourcePath := strings.TrimSuffix(filePath, "\\"+info.Name()) // e.g. charts\!import\folder 1
-			relPath := strings.TrimPrefix(sourcePath, sourceFolder)      // e.g. \folder 1
+			relPath := strings.TrimPrefix(sourcePath, AbsInputRoot)      // e.g. \folder 1
 			relPath = strings.TrimPrefix(relPath, "\\")                  // e.g. folder 1
-			targetPath := outFolder + "\\" + relPath                     // e.g. charts\imported\folder 1
+			targetPath := AbsOutputRoot + "\\" + relPath                 // e.g. charts\imported\folder 1
 
 			fmt.Println("filePath: " + filePath)
 			fmt.Println("sourcePath: " + sourcePath)
-			fmt.Println("sourceRoot: " + sourceFolder)
+			fmt.Println("sourceRoot: " + AbsInputRoot)
 			fmt.Println("relPath: " + relPath)
 			fmt.Println("targetPath: " + targetPath)
 
@@ -166,7 +142,7 @@ func CreatePdfFileList() ([]PdfFileInfo, error) {
 	})
 
 	if walkErr != nil {
-		fmt.Println("Could not import folder " + sourceFolder + ", reason: " + walkErr.Error())
+		fmt.Println("Could not import folder " + AbsInputRoot + ", reason: " + walkErr.Error())
 		return list, walkErr
 	}
 
@@ -206,7 +182,7 @@ func ImportPdfFolder(updateStatusCallback func(string)) error {
 }
 
 func HasImporter() bool {
-	if !fileExists(importerCallerPath) || !fileExists(importerExePath) || !fileExists(importerDllPath) {
+	if !fileExists(importerExePath) || !fileExists(importerDllPath) {
 		fmt.Println("Local importer binaries not found or incomplete!")
 		return false
 	}
@@ -215,57 +191,22 @@ func HasImporter() bool {
 	return true
 }
 
-func DownloadImporter() error {
-	fmt.Println("Downloading Importer...")
-
-	mkDirErr := os.MkdirAll(importerBasePath, os.ModePerm)
-
-	if mkDirErr != nil {
-		fmt.Println("Could not create importer base directory, reason: " + mkDirErr.Error())
-		return mkDirErr
-	}
-
-	if err := downloadFile(importerCallerPath, importerCallerUrl); err != nil {
-		fmt.Println("Could not download importer caller, reason: " + err.Error())
-		return err
-	}
-
-	if err := downloadFile(importerExePath, importerExeUrl); err != nil {
-		fmt.Println("Could not download importer executable, reason: " + err.Error())
-		return err
-	}
-
-	if err := downloadFile(importerDllPath, importerDllUrl); err != nil {
-		fmt.Println("Could not download importer dll, reason: " + err.Error())
-		return err
-	}
-
-	if err := downloadFile(importerLicensePath, importerLicenseUrl); err != nil {
-		fmt.Println("Could not download importer license, reason: " + err.Error())
-		return err
-	}
-
-	fmt.Println("Importer successfully downloaded...")
-
-	return nil
-}
-
 func OpenPdfSourceFolder() {
-	fmt.Println("Trying to open PDF import source folder [" + sourceFolder + "]...")
-	createAndOpenFolder(sourceFolder)
+	fmt.Println("Trying to open PDF import source folder [" + AbsInputRoot + "]...")
+	createAndOpenFolder(AbsInputRoot)
 }
 
 func OpenPdfOutFolder() {
-	fmt.Println("Trying to open PDF import out folder [" + outFolder + "]...")
-	createAndOpenFolder(outFolder)
+	fmt.Println("Trying to open PDF import out folder [" + AbsOutputRoot + "]...")
+	createAndOpenFolder(AbsOutputRoot)
 }
 
 func ClearPdfImportFolder() error {
-	absSourcePath, _ := filepath.Abs(sourceFolder)
+	absSourcePath := AbsInputRoot
 	err := os.RemoveAll(absSourcePath)
 
 	if err != nil {
-		fmt.Println("PDF import source folder [" + sourceFolder + "] could not be cleared, reason: " + err.Error())
+		fmt.Println("PDF import source folder [" + AbsInputRoot + "] could not be cleared, reason: " + err.Error())
 		return err
 	}
 
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"path/filepath"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -11,13 +13,28 @@ import (
 const APP_NAME = "FSKneeboard PDF Importer"
 
 var ParentWindow *fyne.Window
+
+var relLibPath string
+var relInputRoot string
+var relOutputRoot string
+
+var AbsLibPath string
 var AbsInputRoot string
 var AbsOutputRoot string
 
 func main() {
-	flag.StringVar(&AbsInputRoot, "in", "", "specify input root")
-	flag.StringVar(&AbsOutputRoot, "out", "", "specify output root")
+	flag.StringVar(&relLibPath, "lib", ".\\lib", "specify lib path")
+	flag.StringVar(&relInputRoot, "in", "", "specify input root")
+	flag.StringVar(&relOutputRoot, "out", "", "specify output root")
 	flag.Parse()
+
+	AbsLibPath, _ = filepath.Abs(relLibPath)
+	AbsInputRoot, _ = filepath.Abs(relInputRoot)
+	AbsOutputRoot, _ = filepath.Abs(relOutputRoot)
+
+	fmt.Println("AbsLibPath: " + AbsLibPath)
+	fmt.Println("AbsInputRoot: " + AbsInputRoot)
+	fmt.Println("AbsOutputRoot: " + AbsOutputRoot)
 
 	a := app.New()
 	w := a.NewWindow(APP_NAME)
